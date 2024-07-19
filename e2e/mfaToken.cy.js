eb9991227256ad5b6adb62594ed8c953fbcf8c84
anjali-bolai <anjali@mavanisolution.com>
2024-07-19 12:36:58 +0530

    Delete e2e directory

--- e2e/mfaToken.cy.js
+++
@@ -1,6 +0,0 @@
-const { authenticator } = require('otplib');
-
-const secret = 'JZTTAI3XNBPEMU3NGVBXI2ZPJE4TEYLJJB3GWTD5IRRXIMCCLZJA';
-const token = authenticator.generate(secret);
-
-console.log('Generated OTP token:', token);
